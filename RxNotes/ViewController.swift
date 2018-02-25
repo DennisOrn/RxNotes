ebaf91b045029c3edcfae8900104cad9b3c6c888
Dennis Örnberg <dennisornberg@gmail.com>
2018-02-25 13:20:35 +0100

    Added a simple tableview

--- RxNotes/ViewController.swift
+++ RxNotes/ViewController.swift
@@ -6,20 +6,26 @@
 //  Copyright © 2018 Dennis Örnberg. All rights reserved.
 //
 
+import RxSwift
+import RxCocoa
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+
+    let items = Observable.just(["one", "two", "three"])
+
+    let disposeBag = DisposeBag()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        items
+            .bind(to: tableView.rx.items(cellIdentifier: "Cell", cellType: UITableViewCell.self)) {
+                row, element, cell in
+                cell.textLabel?.text = "\(element)"
+            }
+            .disposed(by: disposeBag)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
-
