7744dae10b64e87662fb6f7ef91e80e176bdd30b
Dennis Örnberg <dennisornberg@gmail.com>
2018-02-25 11:10:17 +0100

    Initial commit

---
+++ RxNotes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RxNotes
+//
+//  Created by Dennis Örnberg on 2018-02-25.
+//  Copyright © 2018 Dennis Örnberg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
